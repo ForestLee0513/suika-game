3d20b208f21d6dcd3e18efb6a264af9c83ad9f39
WoolimLee <woolimlee.dev@gmail.com>
2024-04-02 16:51:41 +0900

    플레이어 이동방식 수정, 플레이어에서 바구니 오브젝트 설정 시 자동 이동 영역제한 적용

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float h = 0;
-    Rigidbody2D rb;
+    public GameObject basket;
     public float speed;
+
+    private float leftBaksetBorder;
+    private float rightBaksetBorder;
+    private float halfWidthOfPlayer;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        halfWidthOfPlayer = transform.localScale.x / 2;
+
+        if (basket != null)
+        {
+            Renderer rend = basket.GetComponent<Renderer>();
+            rightBaksetBorder = rend.bounds.max.x - halfWidthOfPlayer;
+            leftBaksetBorder = rend.bounds.min.x + halfWidthOfPlayer;
+        }
     }
 
     void Update()
     {
         h = Input.GetAxisRaw("Horizontal");
-    }
+        if ((h > 0 && transform.position.x >= rightBaksetBorder) || (h < 0 && transform.position.x <= leftBaksetBorder))
+        {
+            return;
+        }
 
-    void FixedUpdate()
-    {
-        rb.velocity = new Vector2(h * speed, 0);
+        transform.Translate(new Vector2(h * speed, 0) * Time.deltaTime);
     }
 }
