0c88b5955e2be762bd05f34b62612f350bfa1594
WoolimLee <woolimlee.dev@gmail.com>
2024-04-04 12:40:15 +0900

    게임오버 조건 트리거 추가

--- Assets/Scripts/WarningZone.cs
+++ Assets/Scripts/WarningZone.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class WarningZone : MonoBehaviour
 {
-    [SerializeField]
-    private float durationTime = 0;
     public float gameOverTriggerTime = 2.0f;
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -27,13 +19,21 @@ public class WarningZone : MonoBehaviour
     {
         if (collision.tag == "Fruit")
         {
-            Debug.Log("타이머 종료");
+            // 방향 감지 후 아래로 갔을 때만 게임오버 처리없이 타이머 종료
+            float lastCollisionPosition = collision.transform.position.y;
+            float warningZonePosition = transform.position.y;
+
+            if(lastCollisionPosition > warningZonePosition)
+            {
+                Debug.Log("즉시 게임오버!!!!");
+            }
+
             CancelInvoke("GameOverTimer");
         }
     }
 
 
-    // 테스트용 함수. 추후 게임 매니저 등으로 통해 게임오버 제어 예정.
+    // 테스트용 함수. 추후 게임 매니저(스테이지) 등으로 통해 게임오버 제어 예정.
     void GameOverTimer()
     {
         Debug.Log("게임 오버");
