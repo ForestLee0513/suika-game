afc7500081ec9cbd55a5a23612ff3cfb5ab7c010
WoolimLee <woolimlee.dev@gmail.com>
2024-04-05 14:03:31 +0900

    병합 이동모션 적용 안되는 문제 해결

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Fruit : MonoBehaviour
@@ -17,7 +19,7 @@ public class Fruit : MonoBehaviour
         circleCollider = GetComponent<CircleCollider2D>();
     }
 
-    void OnCollisionStay2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Fruit")
         {
@@ -27,17 +29,10 @@ public class Fruit : MonoBehaviour
                 Vector3 myPosition = transform.position;
                 Vector3 collisionFruitPosition = collision.transform.position;
 
-                if(myPosition.y < collisionFruitPosition.y || (myPosition.y == collisionFruitPosition.y && myPosition.x > collisionFruitPosition.x))
+                if (myPosition.y < collisionFruitPosition.y || (myPosition.y == collisionFruitPosition.y && myPosition.x > collisionFruitPosition.x))
                 {
                     collisionFruit.Hide(transform.position);
-                    if (FruitPrefabManager.Instance.fruitPrefabs.Length > level - 1)
-                    {
-                        LevelUp();
-                    }
-                    else
-                    {
-                        Destroy(gameObject);
-                    }
+                    LevelUp();
                 }
             }
         }
@@ -45,24 +40,27 @@ public class Fruit : MonoBehaviour
 
     public void Hide(Vector3 targetPos)
     {
+        Debug.Log("collide other");
         isMerge = true;
         rb.simulated = false;
         circleCollider.enabled = false;
-        StartCoroutine(HideRoutine(targetPos));
+        StartCoroutine(HideRoutine(targetPos, 6.0f));
     }
 
-    IEnumerator HideRoutine(Vector3 targetPos)
+    IEnumerator HideRoutine(Vector3 targetPos, float mergeSpeed)
     {
-        int frameCount = 0;
+        float scale = (transform.localScale.x + transform.localScale.y + transform.localScale.z) / 3;
+        float speedAtScale = scale * mergeSpeed;
 
-        while (frameCount < 20)
+        while (Vector3.Distance(transform.position, targetPos) > speedAtScale * Time.deltaTime)
         {
-            frameCount++;
-            transform.position = Vector3.Lerp(transform.position, targetPos, 0.1f);
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speedAtScale * Time.deltaTime);
             yield return null;
         }
 
+        transform.position = targetPos;
         isMerge = false;
+
         Destroy(gameObject);
     }
 
@@ -72,7 +70,11 @@ public class Fruit : MonoBehaviour
 
         rb.velocity = Vector2.zero;
         rb.angularVelocity = 0;
+        
         Destroy(gameObject);
-        Instantiate(FruitPrefabManager.Instance.fruitPrefabs[level + 1], transform.position, Quaternion.identity);
+        if(level + 1 < FruitPrefabManager.Instance.fruitPrefabs.Length)
+        {
+            Instantiate(FruitPrefabManager.Instance.fruitPrefabs[level + 1], transform.position, Quaternion.identity);
+        }
     }
 }
