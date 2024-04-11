1654c3df6659e242be1345e47973864ef8a0a8aa
WoolimLee <woolimlee.dev@gmail.com>
2024-04-11 11:02:11 +0900

    씬 불러오는 방식 비동기로 변경

--- Assets/Scripts/UIEvents/TitleUIEvents.cs
+++ Assets/Scripts/UIEvents/TitleUIEvents.cs
@@ -7,6 +7,18 @@ public class TitleUIEvents : MonoBehaviour
 {
     public void MoveToInGame()
     {
-        SceneManager.LoadScene(1);
+        StartCoroutine(nameof(LoadInGameAsync));
+    }
+
+    // 에셋이 많아져서 로딩이 생긴다면 로딩바 구현 예정. (그 전에 생기지 않는게 좋겠지만..)
+    IEnumerator LoadInGameAsync()
+    {
+        AsyncOperation nextScene = SceneManager.LoadSceneAsync(1);
+
+        // load per frame;
+        while (!nextScene.isDone)
+        {
+            yield return null;
+        }
     }
 }
